1f921918ffbdf9a53f2a19d3e10622c09faaf057
Harsh Kumar Saini <150950663+Culebrin19@users.noreply.github.com>
2024-11-06 14:02:06 +0100

    proves

--- js/script.js
+++ js/script.js
@@ -1,158 +1,218 @@
-document.getElementById("errors").value = "Prova d'error";
-    var torn = true; // boolean, si es fa click a una operacio, canvia de true (operador 1) a false (operador 2)
-    var operador1 = "";
-    var operador2 = "";
-    var resultat = "";
-    var operacio = "";
+var torn = true; // boolean, si es fa click a una operacio, canvia de true (operador 1) a false (operador 2)
+var operador1 = 0;
+var operador2 = 0;
+var resultat = 0;
+var operacio = "";
+var valorInicial = 0;
 
-    // FALTA IMPRIMIR 0 AL INICI DE LA CALCULADORA
-    // TAMBÉ QUE AL FER CLICK A UNA OPERACIO NO ES BORRIN ELS VALORS DE L'ALTRE OPERANT
-    // FALTA QUE AL FER CLICK A = MOSTRI EL RESULTAT
+// var operand1 = "0";
 
-    function tractarUn(){
-      if (torn) {
-        operador1 = operador1 + "1";
-        resultat = document.getElementById("resultat");
-        resultat.value = operador1;
-      } else {
-        operador2 = operador2 + "1";
-        resultat = document.getElementById("resultat");
-        resultat.value = operador2;
-      }
-    }
+// document.addEventListener("DOMContentLoaded", function () {
+//   operand1 = "0";
+//   document.getElementById("resultat").value = operand1;
+// });
 
-    function tractarDos(){
-      if (torn) {
-        operador1 = operador1 + "2";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador1;
-      } else {
-        operador2 = operador2 + "2";
-        resultat = document.getElementById("resultat");
-        resultat.value = operador2;
-      }
-    }
+// FALTA IMPRIMIR 0 AL INICI DE LA CALCULADORA
+// TAMBÉ QUE AL FER CLICK A UNA OPERACIO NO ES BORRIN ELS VALORS DE L'ALTRE OPERANT
+// FALTA QUE AL FER CLICK A = MOSTRI EL RESULTAT
 
-    function tractarTres(){
-      if (torn) {
-        operador1 = operador1 + "3";
-        resultat = document.getElementById("resultat");
-        resultat.value = operador1;
-      } else {
-        operador2 = operador2 + "3";
-        resultat = document.getElementById("resultat");
-        resultat.value = operador2;
-      }
-    }
+mostrarZero();
+function mostrarZero() {
+  document.getElementById("resultat").value = valorInicial; // MIRAR ON FICAR-HO DINS D'UNA FUNCIO
+}
 
-    function tractarQuatre(){
-      if (torn) {
-        operador1 = operador1 + "4";
-        resultat = document.getElementById("resultat");
-        resultat.value = operador1;
-      } else {
-        operador2 = operador2 + "4";
-        resultat = document.getElementById("resultat");
-        resultat.value = operador2;
-      }
-    }
+// errors();
+function errors() {
+  document.getElementById("errors").value = "Prova d'error";
+}
 
-    function tractarCinc(){
-      if (torn) {
-        operador1 = operador1 + "5";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador1;
-      } else {
-        operador2 = operador2 + "5";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador2;
-      }
-    }
 
-    function tractarSis(){
-      if (torn) {
-        operador1 = operador1 + "6";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador1;
-      } else {
-        operador2 = operador2 + "6";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador2;
-      }
-    }
+function opcio(){ // Mirar que ficar en les condicions, perque no igualo cap valor
+  if(document.getElementById("+")){
+    operacio="+";
+    document.write("sumar");
+  }else if(document.getElementById("-")){
+    operacio="-";
+    document.write("restar");
+  }else if(document.getElementById("*")){
+    operacio="*";
+    document.write("mutiplicar");
+  }else if(document.getElementById("/")){
+    operacio="/";
+    document.write("dividir");
+  }else{
+    console.error("Error de operador");
+  }
+  canviarTorn();
+}
 
-    function tractarSet(){
-      if (torn) {
-        operador1 = operador1 + "7";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador1;
-      } else {
-        operador2 = operador2 + "7";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador2;
-      }
-    }
 
-    function tractarVuit(){
-      if (torn) {
-        operador1 = operador1 + "8";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador1;
-      } else {
-        operador2 = operador2 + "8";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador2;
-      }
-    } 
+function canviarTorn() {
+  // MIRAR COM INDICAR-LI A QUIN TIPUS D'OPERACIO ES FA CLICK
+  if (operacio == "+") {
+    torn = !torn;
+    document.getElementById("errors").value = "Prova d'error sumar";
+    // resultat = operador1 + operador2;
+  } else if (operacio == "-") {
+    torn = !torn;
+    document.getElementById("errors").value = "Prova d'error restar";
+    resultat = operador1 - operador2;
+  } else if (operacio == "*") {
+    torn = !torn;
+    document.getElementById("errors").value = "Prova d'error mutiplicar";
+    resultat = operador1 * operador2;
+  } else if (operacio == "/") {
+    torn = !torn;
+    document.getElementById("errors").value = "Prova d'error dividir";
+    resultat = operador1 / operador2;
+  }
+  // torn = !torn; // mirara si canvia el valor de torn al contrari
+  document.getElementById("resultat").value = resultat;
+}
 
-    function tractarNou(){
-      if (torn) {
-        operador1 = operador1 + "9";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador1;
-      } else {
-        operador2 = operador2 + "9";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador2;
-      }
-    }
 
-    function tractarZero(){
-      if (torn) {
-        operador1 = operador1 + "0";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador1;
-      } else {
-        operador2 = operador2 + "0";
-        pantalla = document.getElementById("resultat");
-        pantalla.value = operador2;
-      }
-    }
-    
+function tractarUn() {
+  if (torn) {
+    operador1 = operador1 + "1";
+    resultat = document.getElementById("resultat");
+    resultat.value = operador1;
+  } else {
+    operador2 = operador2 + "1";
+    resultat = document.getElementById("resultat");
+    resultat.value = operador2;
+  }
+}
 
-    function calcular() {
-      // mirar que ficar, operacio està per estar
-      if (operacio == "+") {
-      resultat = operador1 + operador2;
-      } else if (operacio == "-") {
-      resultat = operador1 - operador2;
-      } else if (operacio == "*") {
-      resultat = operador1 * operador2;
-      } else if (operacio == "/") {
-      resultat = operador1 / operador2;
-      }
-      document.getElementById("resultat").value = resultat;
-      torn = !torn; // mirara si canvia el valor de torn de true a false
-    }
+function tractarDos() {
+  if (torn) {
+    operador1 = operador1 + "2";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador1;
+  } else {
+    operador2 = operador2 + "2";
+    resultat = document.getElementById("resultat");
+    resultat.value = operador2;
+  }
+}
 
-    // function resultat(){
-    //   pantalla = document.getElementById("resultat");
-    //   pantalla.value = resultat;
-    // }
+function tractarTres() {
+  if (torn) {
+    operador1 = operador1 + "3";
+    resultat = document.getElementById("resultat");
+    resultat.value = operador1;
+  } else {
+    operador2 = operador2 + "3";
+    resultat = document.getElementById("resultat");
+    resultat.value = operador2;
+  }
+}
 
-    function esborrar() {
-      operador1 = "";
-      operador2 = "";
-      
-      pantalla.value = "";
-    }
+function tractarQuatre() {
+  if (torn) {
+    operador1 = operador1 + "4";
+    resultat = document.getElementById("resultat");
+    resultat.value = operador1;
+  } else {
+    operador2 = operador2 + "4";
+    resultat = document.getElementById("resultat");
+    resultat.value = operador2;
+  }
+}
+
+function tractarCinc() {
+  if (torn) {
+    operador1 = operador1 + "5";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador1;
+  } else {
+    operador2 = operador2 + "5";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador2;
+  }
+}
+
+function tractarSis() {
+  if (torn) {
+    operador1 = operador1 + "6";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador1;
+  } else {
+    operador2 = operador2 + "6";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador2;
+  }
+}
+
+function tractarSet() {
+  if (torn) {
+    operador1 = operador1 + "7";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador1;
+  } else {
+    operador2 = operador2 + "7";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador2;
+  }
+}
+
+function tractarVuit() {
+  if (torn) {
+    operador1 = operador1 + "8";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador1;
+  } else {
+    operador2 = operador2 + "8";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador2;
+  }
+}
+
+function tractarNou() {
+  if (torn) {
+    operador1 = operador1 + "9";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador1;
+  } else {
+    operador2 = operador2 + "9";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador2;
+  }
+}
+
+function tractarZero() {
+  if (torn) {
+    operador1 = operador1 + "0";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador1;
+  } else {
+    operador2 = operador2 + "0";
+    pantalla = document.getElementById("resultat");
+    pantalla.value = operador2;
+  }
+}
+
+
+function resultat(){
+  resultat = operador1 + operador2;
+  document.getElementById("resultat").value=resultat;
+}
+
+// function resultat(){
+//   pantalla = document.getElementById("resultat");
+//   pantalla.value = resultat;
+// }
+
+function esborrar() {
+  operador1 = "";
+  operador2 = "";
+
+  pantalla.value = "";
+}
+
+function goBack() {
+  window.history.back();
+}
+
+function goForward() {
+  window.history.forward;
+}
