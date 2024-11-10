308cc67c1658c495b9d4e0397d4de437d8018407
Harsh Kumar Saini <150950663+Culebrin19@users.noreply.github.com>
2024-11-10 11:40:53 +0100

    enllaçosv2

--- js/script.js
+++ js/script.js
@@ -22,9 +22,9 @@ function mostrar() {
   localStorage.setItem("tipusCalculadora", tipus);
 
   if (tipus == "normal") {
-    window.location.href = `/html/calculadora.html?tipus=${tipus}`;
+    window.location.href = "/html/calculadora.html";
   } else if (tipus == "cientifica") {
-    window.location.href = `/html/calculadoraCientifica.html?tipus=${tipus}`;
+    window.location.href = "/html/calculadoraCientifica.html";
   }
 
   alert("S'ha guardat el nom i el tipus de calculadora");
